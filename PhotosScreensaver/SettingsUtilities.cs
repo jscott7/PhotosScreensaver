0b4f24cce94c0594fe76dc4710bb76008e52c6f3
Jonathan Scott <jscott7@users.noreply.github.com>
2023-06-08 19:55:30 +0100

    Apply SupportedOsPlatform attribute

--- PhotosScreensaver/SettingsUtilities.cs
+++ PhotosScreensaver/SettingsUtilities.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.Win32;
+using System.Runtime.Versioning;
 
 namespace PhotosScreensaver
 {
     public class SettingsUtilities
     {
         private static readonly string _keyName = "SOFTWARE\\JscoPhotoScreenSaver";
+
         /// <summary>
         /// Saves a setting to the Registry
         /// </summary>
         /// <param name="name"></param>
         /// <param name="value"></param>
+        [SupportedOSPlatform("windows")]
         public static void SaveSetting(string name, object value)
         {
             var key = Registry.CurrentUser.CreateSubKey(_keyName);
@@ -25,6 +28,7 @@ namespace PhotosScreensaver
         /// </summary>
         /// <param name="name"></param>
         /// <returns></returns>
+        [SupportedOSPlatform("windows")]
         public static object LoadSetting(string name)
         {
             // Get the value stored in the Registry  
